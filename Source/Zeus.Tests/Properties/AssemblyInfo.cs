f356ba1dd117c6a80a529f63c546e89333fcbda7
Tim Jones <tim@timjones.tw>
2012-03-17 03:08:47 +0800

    Should be right this time...

--- Source/Zeus.Tests/Properties/AssemblyInfo.cs
+++ Source/Zeus.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// NOTE: Shared assembly information is specified in SharedAssemblyInfo.cs
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyCompany("Sound in Theory Ltd")]
+[assembly: AssemblyProduct("Zeus")]
+[assembly: AssemblyCopyright("Copyright ©2012 Sound in Theory Ltd")]
+[assembly: AssemblyTrademark("sitdap.com")]
+[assembly: ComVisible(false)]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -9,6 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Zeus.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0dbf2f2d-9d64-4a4a-b0ce-fbe9902b31b0")]
